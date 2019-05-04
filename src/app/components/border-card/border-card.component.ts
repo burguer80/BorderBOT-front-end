978ce687346a970a90073394110cdb15858b7c88
Sergio Burgueño <burguer@gmail.com>
2019-05-04 12:50:10 -0700

    basic setup

---
+++ src/app/components/border-card/border-card.component.ts
@@ -0,0 +1,113 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Border} from '../../interfaces/border';
+
+@Component({
+  selector: 'app-border-card',
+  template: `
+    <mat-card class="border-card">
+      <mat-card-header>
+        <mat-card-title>{{border.name}} - {{border.crossing_name}} </mat-card-title>
+        <mat-card-subtitle>{{border.hours}}</mat-card-subtitle>
+      </mat-card-header>
+      <mat-card-content>
+        <mat-grid-list cols="5" rowHeight="35">
+          <div class="table-headers">
+            <mat-grid-tile></mat-grid-tile>
+            <mat-grid-tile>Standard</mat-grid-tile>
+            <mat-grid-tile>Ready</mat-grid-tile>
+            <mat-grid-tile>Sentri</mat-grid-tile>
+            <mat-grid-tile>Fast</mat-grid-tile>
+          </div>
+
+          <mat-grid-tile class="table_icon">
+            <mat-icon>directions_walk</mat-icon>
+          </mat-grid-tile>
+          <div class="table_value">
+            <mat-grid-tile>{{formatValue(border.data?.pedestrian?.standard_lanes?.delay_minutes)}}</mat-grid-tile>
+            <mat-grid-tile>{{formatValue(border.data?.pedestrian?.ready_lanes?.delay_minutes)}}</mat-grid-tile>
+            <mat-grid-tile>{{formatValue(border.data?.pedestrian?.NEXUS_SENTRI_lanes?.delay_minutes)}}</mat-grid-tile>
+            <mat-grid-tile>{{formatValue(border.data?.pedestrian?.FAST_lanes?.delay_minutes)}}</mat-grid-tile>
+          </div>
+
+          <mat-grid-tile class="table_icon">
+            <mat-icon>directions_car</mat-icon>
+          </mat-grid-tile>
+          <div class="table_value">
+            <mat-grid-tile>{{formatValue(border.data?.passenger?.standard_lanes?.delay_minutes)}}</mat-grid-tile>
+            <mat-grid-tile>{{formatValue(border.data?.passenger?.ready_lanes?.delay_minutes)}}</mat-grid-tile>
+            <mat-grid-tile>{{formatValue(border.data?.passenger?.NEXUS_SENTRI_lanes?.delay_minutes)}}</mat-grid-tile>
+            <mat-grid-tile>{{formatValue(border.data?.passenger?.FAST_lanes?.delay_minutes)}}</mat-grid-tile>
+          </div>
+
+          <mat-grid-tile class="table_icon">
+            <mat-icon>local_shipping</mat-icon>
+          </mat-grid-tile>
+          <div class="table_value">
+            <mat-grid-tile>{{formatValue(border.data?.commercial?.standard_lanes?.delay_minutes)}}</mat-grid-tile>
+            <mat-grid-tile>{{formatValue(border.data?.commercial?.ready_lanes?.delay_minutes)}}</mat-grid-tile>
+            <mat-grid-tile>{{formatValue(border.data?.commercial?.NEXUS_SENTRI_lanes?.delay_minutes)}}</mat-grid-tile>
+            <mat-grid-tile>{{formatValue(border.data?.commercial?.FAST_lanes?.delay_minutes)}}</mat-grid-tile>
+          </div>
+        </mat-grid-list>
+      </mat-card-content>
+      <mat-card-footer>
+        <p>Updated: <b>{{border?.taken_at |  date:'medium'}}</b></p>
+      </mat-card-footer>
+    </mat-card>
+  `,
+  styles: [`
+    mat-card-header {
+      height: 110px;
+      background-image: linear-gradient(to top, #e6e9f0 0%, #eef1f5 100%);
+    }
+
+    mat-card-title {
+      margin-top: 10px;
+    }
+
+    mat-grid-list {
+      top: -30px;
+    }
+
+    mat-card-footer {
+      margin-left: 5px;
+      font-size: 12px;
+    }
+
+    .border-card {
+      max-width: 320px;
+      min-width: 320px;
+    }
+
+    .table-headers {
+      /*color: white;*/
+      font-size: 14px;
+      font-weight: bold;
+    }
+
+    .table_value {
+      font-size: 14px;
+      opacity: 0.7;
+    }
+  `]
+
+})
+export class BorderCardComponent implements OnInit {
+  @Input() border: Border;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  formatValue(value) {
+    const cleaned_value = value || value === 0 ? value.toString().trim() : null;
+    let formatted_value = '-';
+    if (cleaned_value) {
+      formatted_value = cleaned_value + 'm';
+    }
+    return formatted_value;
+  }
+
+}
---
+++ src/app/interfaces/border.ts
@@ -0,0 +1,12 @@
+export interface Border {
+  id: number;
+  name: string;
+  hours: string;
+  opens_at: number;
+  closed_at: number;
+  border_name: string;
+  crossing_name: string;
+  status: string;
+  taken_at: string;
+  data: any;
+}
