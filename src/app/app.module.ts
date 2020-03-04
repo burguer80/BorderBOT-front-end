dfbca8bf054d1bf28dc2deeb887841f6fa1f5669
Sergio Burgueño <burguer@gmail.com>
2020-03-03 21:48:54 -0800

    update to angular 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,15 @@ import {AngularFireModule} from '@angular/fire';
 import {AngularFireDatabaseModule} from '@angular/fire/database';
 import {environment} from '../environments/environment';
 
-import {
-  MatButtonModule,
-  MatCardModule,
-  MatExpansionModule,
-  MatGridListModule,
-  MatIconModule,
-  MatProgressSpinnerModule,
-  MatTableModule,
-  MatInputModule, MatSelectModule
-} from '@angular/material';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatSelectModule} from '@angular/material/select';
+import {MatTableModule} from '@angular/material/table';
 import {MatFormFieldModule} from '@angular/material/form-field';
 
 // Components
