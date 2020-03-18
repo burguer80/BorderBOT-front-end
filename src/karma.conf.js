106dbc035fbf45a217a170f030c4e2b9fe0cf901
Sergio Burgueño <burguer@gmail.com>
2020-03-17 20:29:51 -0700

    re

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -26,6 +26,14 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    restartOnFileChange: true,
+    restartOnFileChange: true,
+    customLaunchers: {
+      ChromeHeadlessCustom: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu']
+      }
+    },
   });
 };
