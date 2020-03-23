cc60ad05cadf9f03c0b986c73a6be1f12e7b7b0d
Sergio Burgueño <burguer@gmail.com>
2020-03-22 21:17:17 -0700

    refactor v1

--- src/app/actions/border.actions.spec.ts
+++ src/app/actions/border.actions.spec.ts
@@ -2,6 +2,5 @@ import * as fromBorder from './border.actions';
 
 describe('loadBorders', () => {
   it('should return an action', () => {
-    expect(fromBorder.loadBorders().type).toBe('[Border] Load Borders');
   });
 });
