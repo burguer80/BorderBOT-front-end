04f151c35a4ec5747442ea0d26885dbfcbe132e4
Sergio Burgueño <burguer@gmail.com>
2020-03-22 21:36:10 -0700

    refactor v1

--- src/app/reducers/borders.reducer.spec.ts
+++ src/app/reducers/borders.reducer.spec.ts
@@ -1,11 +1,11 @@
-import {reducer, initialState} from './borders.reducer';
+import {BorderReducer, initialState} from './borders.reducer';
 
 describe('Borders Reducer', () => {
   describe('an unknown action', () => {
     it('should return the previous state', () => {
       const action = {} as any;
 
-      const result = reducer(initialState, action);
+      const result = BorderReducer(initialState, action);
 
       expect(result).toBe(initialState);
     });
